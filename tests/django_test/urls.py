c5edac2164267721a1c92eb760fdc53c69fc8978
ppr.vitaly <ppr.vitaly@a0fd97e0-1548-0410-832c-cd162a569da5>
2008-03-25 20:18:27 +0000

    tests for forms

--- tests/django_test/urls.py
+++ tests/django_test/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('django_test.views',
-    ('^form1/$', 'form1'),
+    ('^regular_form/$', 'regular_form'),
+    ('^regular_form/(?P<id>\d+)/$', 'regular_form_edit'),
+    ('^model_form/$', 'model_form'),
+    ('^model_form/(?P<id>\d+)/$', 'model_form_edit'),
+    
 )
--- tests/django_test/views.py
+++ tests/django_test/views.py
@@ -1,21 +1,68 @@
 from django import newforms as forms
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from money.contrib.django.forms.fields import MoneyField
 from money import Money
+
+
+#regular form
 
 class TestForm(forms.Form):
     price = MoneyField()
 
 
+#ModelForm
+from django_test.models import Entity    
+class TestModelForm(forms.ModelForm):
+    class Meta:
+        model = Entity
 
-def form1(request):
+
+
+
+def regular_form(request):
     if request.method == 'POST':
         form = TestForm(request.POST)
         print form.is_valid()
         if form.is_valid():
             price = form.cleaned_data['price']
-            print price
-            return render_to_response('form1.html', {'price':price} )
+            return render_to_response('form.html', {'price':price} )
     else:
         form = TestForm()
-    return  render_to_response('form1.html', {'form':form} )
+    return  render_to_response('form.html', {'form':form} )
+
+def regular_form_edit(request, id):
+    instance = get_object_or_404(Entity, pk=id)
+    if request.method == 'POST':
+        form = TestForm(request.POST, initial={'price':instance.price})
+        print form.is_valid()
+        if form.is_valid():
+            price = form.cleaned_data['price']
+            return render_to_response('form.html', {'price':price} )
+    else:
+        form = TestForm(initial={'price':instance.price})
+    return  render_to_response('form.html', {'form':form} )
+
+def model_form(request):
+    if request.method == 'POST':
+        form = TestModelForm(request.POST)
+        print form.is_valid()
+        if form.is_valid():
+            price = form.cleaned_data['price']
+            form.save()
+            return render_to_response('form.html', {'price':price} )
+    else:
+        form = TestModelForm()
+    return  render_to_response('form.html', {'form':form} )
+
+def model_form_edit(request, id):
+    instance = get_object_or_404(Entity, pk=id)
+    if request.method == 'POST':
+        form = TestModelForm(request.POST, instance=instance)
+        print form.is_valid()
+        if form.is_valid():
+            price = form.cleaned_data['price']
+            form.save()
+            return render_to_response('form.html', {'price':price} )
+    else:
+        form = TestModelForm(instance=instance)
+    return  render_to_response('form.html', {'form':form} )
